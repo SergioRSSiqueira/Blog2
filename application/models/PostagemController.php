b683125b362b3bf6a436f471e76173e170d5889d
Sergio Ricardo de Sousa Siqueira <sergio.siqueira@academico.ifrn.edu.br>
2019-03-21 15:54:59 -0300

    fazendo atualizações

--- application/models/PostagemController.php
+++ application/models/PostagemController.php
@@ -8,4 +8,12 @@
             $this->load->view('postagem/index',$parametros);
             $this->load->view('rodape');
         }
+    
+     	public function visualizar(){
+            $parametros['titulo'] = 'Postagens';
+            $parametros['postagens'] =  $this->Postagem->get();
+            $this->load->view('cabecalho',$parametros);
+            $this->load->view('postagem/visualizar',$parametros);
+            $this->load->view('rodape');
+        }
     }
